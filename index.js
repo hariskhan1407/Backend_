43e2d6fc4f1965ba60b808d4821b71e5d08be9ca
Haris <hariskhan1407@gmail.com>
2025-03-10 04:00:03 +0530

    github data added

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 require('dotenv').config()
 //to make production ready app
 
+const githubData = {
+    
+        "login": "hariskhan1407",
+        "id": 148951890,
+        "node_id": "U_kgDOCODTUg",
+        "avatar_url": "https://avatars.githubusercontent.com/u/148951890?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/hariskhan1407",
+        "html_url": "https://github.com/hariskhan1407",
+        "followers_url": "https://api.github.com/users/hariskhan1407/followers",
+        "following_url": "https://api.github.com/users/hariskhan1407/following{/other_user}",
+        "gists_url": "https://api.github.com/users/hariskhan1407/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/hariskhan1407/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/hariskhan1407/subscriptions",
+        "organizations_url": "https://api.github.com/users/hariskhan1407/orgs",
+        "repos_url": "https://api.github.com/users/hariskhan1407/repos",
+        "events_url": "https://api.github.com/users/hariskhan1407/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/hariskhan1407/received_events",
+        "type": "User",
+        "user_view_type": "public",
+        "site_admin": false,
+        "name": "Haris Khan",
+        "company": null,
+        "blog": "",
+        "location": null,
+        "email": null,
+        "hireable": null,
+        "bio": "Android Developer\r\n",
+        "twitter_username": null,
+        "public_repos": 9,
+        "public_gists": 0,
+        "followers": 1,
+        "following": 2,
+        "created_at": "2023-10-25T08:16:45Z",
+        "updated_at": "2025-03-08T23:27:53Z"
+      }
+
+
 const express = require('express')
 //express import is done
 const app = express()
@@ -16,6 +54,9 @@ app.get('/login',(req,res)=>{
 })
 app.get('/youtube',(req,res)=>{
     res.send('<h2>Youtube kholna chah re ho kya be!😂</h2>')
+})
+app.get('/github',(req,res)=>{
+    res.json(githubData)
 })
 app.listen(process.env.PORT,()=>{
     console.log(`Example app listen on port ${process.env.PORT}`)
